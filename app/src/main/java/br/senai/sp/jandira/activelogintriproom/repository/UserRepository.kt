a9bbd20f75a519229c167d5d12f2181e8603aeea
MATHEUS ALVES REIS DA SILVA <22282211@dsn127m1247444.cfp127aluno.local>
2023-04-04 16:51:39 -0300

    update db kotlin

---
+++ app/src/main/java/br/senai/sp/jandira/activelogintriproom/repository/UserRepository.kt
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.activelogintriproom.repository
+
+import android.content.Context
+import br.senai.sp.jandira.activelogintriproom.dao.TripDb
+import br.senai.sp.jandira.activelogintriproom.model.User
+
+class UserRepository(context: Context) {
+
+    private val db = TripDb.getDatabase(context)
+
+    fun save(user: User): Long {
+        return db.userDao().save(user)
+    }
+
+}
